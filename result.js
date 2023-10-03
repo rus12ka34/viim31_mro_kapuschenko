41f3c111dfa573a0a694fd8e3fde00b7a24f087f
ruska <ruska35@mail.ru>
2023-10-03 19:05:09 +0300

    circle -> oval

--- result.js
+++ result.js
@@ -5,7 +5,6 @@ const { createCanvas } = require('canvas');
 const result = fs.readFileSync('result.json', { encoding: 'utf8', flag: 'r' });
 const resultJson = JSON.parse(result);
 
-
 const width = 1000;
 const height = 1000;
 
@@ -15,19 +14,15 @@ const context = canvas.getContext("2d");
 context.fillStyle = "#cccccc";
 context.fillRect(0, 0, width, height);
 
-
-context.fillStyle = "#111111";
 resultJson.map((classItem) => {
     const { points } = classItem;
 
     points.map((point) => {
-        const { setX, setY } = point;
-
-        // console.log(setX, setX)
-        context.fillRect(setX*100, 1000 - setY*100, 4, 4);
+        const { myX, myY, hex } = point;
+        context.fillStyle = hex;
+        context.fillRect(myX*100, 1000 - myY*100, 4, 4);
     });
 })
-
 
 const buffer = canvas.toBuffer("image/png");
 fs.writeFileSync("image.png", buffer);
--- start.js
+++ start.js
@@ -3,35 +3,26 @@ const commonData = fs.readFileSync('date/commonData.txt', { encoding: 'utf8', fl
 const clasters = fs.readFileSync('date/clasters.txt', { encoding: 'utf8', flag: 'r' });
 
 const arr = JSON.parse(clasters);
-const common = JSON.parse(commonData);
 
-const getRandomArbitrary = (min, max) => {
-    return Math.random() * (max - min) + min;
-}
+item = "77FF33FF"
 
+const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
 
-const checkPoint = ({ x1, x2, R, setX, setY, a, b }) => {
-
-    return ((Math.pow(setX, 2) / Math.pow(a, 2))) + ((Math.pow(setY, 2) / Math.pow(b, 2))) <= 1
-
-    // return (Math.pow((setX - x1), 2) + Math.pow((setY - x2), 2)) <= Math.pow(R, 2);
-};
+const checkPoint = ({ x1, x2, myX, myY, a, b }) => (((myX - x1) / a)**2) + (((myY - x2) / b)**2) <= 1;
 
 // генерация точек
 let index = 0;
 let points = [];
-while (index <= 10000) {
+while (index <= 100000) {
     points.push({ x1: getRandomArbitrary(0, 10).toFixed(2), x2: getRandomArbitrary(0, 10).toFixed(2) })
     ++index;
 }
 
 
 let verificationPoints = []
-
-
-
 arr.map((classItem, index) => {
-    const { x1, x2, R, a, b } = classItem;
+    const { x1, x2, a, b, hex, itemsCount } = classItem;
+    let counter = 0;
 
     verificationPoints.push({
         arrItem: index,
@@ -39,19 +30,16 @@ arr.map((classItem, index) => {
     })
 
     points.map((item) => {
-        const { x1: setX, x2: setY } = item;
+        const { x1: myX, x2: myY } = item;
 
-        if (checkPoint({ x1, x2, R, setX, setY, a, b })) {
-            verificationPoints[index].points.push({ setX, setY })
+        if (checkPoint({ x1, x2, myX, myY, a, b }) && counter <= +itemsCount) {
+            verificationPoints[index].points.push({ myX, myY, hex })
+            counter++
         }
-        // else console.log('false >>> ', item)
     });
 });
 
 let data = JSON.stringify(verificationPoints);
 fs.writeFileSync('result.json', data);
-
-
-// console.log(verificationPoints);
 
 
