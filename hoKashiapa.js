c90494c5cbad0b3c4a214079941b7472de5dd47d
ruska <ruska35@mail.ru>
2023-12-17 21:56:49 +0300

    Хо-Кашьяпа готово

--- hoKashiapa.js
+++ hoKashiapa.js
@@ -1,99 +1,126 @@
 const fs = require('fs');
-
-
 const result = fs.readFileSync('result.json', { encoding: 'utf8', flag: 'r' });
 const resultJson = JSON.parse(result);
+const { createCanvas } = require('canvas');
+
+// const SVM = require('ml-svm');
+const math = require('mathjs');
 
 
-const transpose = matrix => matrix[0].map((col, i) => matrix.map(row => row[i]));
+// let __features = [];
+// let __labels = [];
+//
+// resultJson.forEach((item) => {
+//     item.points.forEach((point, i) => {
+//         __labels.push(item.arrItem === 0 ? -1 : 1)
+//         __features.push([+point.myX, +point.myY])
+//     })
+// })
 
-const multiplyMatrix = (A, B) => {
-    var rowsA = A.length,
-        colsA = A[0].length,
-        rowsB = B.length,
-        colsB = B[0].length,
-        C = [];
-    if (colsA != rowsB) return false;
-    for (var i = 0; i < rowsA; i++) C[i] = [];
-    for (var k = 0; k < colsB; k++) {
-        for (var i = 0; i < rowsA; i++) {
-            var t = 0;
-            for (var j = 0; j < rowsB; j++) t += A[i][j] * B[j][k];
-            C[i][k] = t;
+
+
+function hoKashyap(X, y, a = 0.1, max = 10000) {
+
+
+    let _X = X.map((item) => [ ...item, 1 ] );
+    let w = _X[0].map((item) => 0);
+    let b = 0;
+
+
+
+
+    for (let i = 0; i < max; i++) {
+        const matrix_w = math.matrix(w)
+        const matrix_X = math.matrix(_X)
+
+        let z = math.multiply(matrix_X, matrix_w)
+        z = math.add(z, b)
+
+        let e = z._data.map((item, i) => y[i] - (item < 0 ? -1 : 1));
+
+
+        // console.log(e);
+        let _w = math.multiply(a, math.multiply(math.transpose(matrix_X), e));
+        w = w.map((item, i) => item + _w._data[i]);
+        b += a * e.reduce((acc, item) => acc + item, 0);
+
+        if (!e.find((item) => item !== 0)){
+            break;
+        }
+
+    }
+
+    return { w, b }
+}
+
+
+function getRandomMatrix(samples, features) {
+    let arr = [];
+    for (let i = 0; i < samples; i++) {
+        arr[i] = [];
+        for (let j = 0; j < features; j++) {
+            arr[i][j] = Math.random() * 10 - 5;
         }
     }
-    return C;
-}
+    return arr;
+};
 
-const MatrixPow = (A, n) => {
-    if (n === 1) return A;
-    else return multiplyMatrix(A, MatrixPow(A, n - 1));
-}
-
-// console.log('resultJson >>> ', resultJson);
-
-// a
-
-let x = {
-    x1: [1, 2, 1],
-    x2: [0, 2, 1],
-    x3: [-1, -3, -1],
-    x4: [-3, -2, -1],
-}
-// b
-
-const V = [...Object.values(x).map((x) => x)];
-
-// v
-
-// не закочил возведение в степень (-1)
-const _V = ( multiplyMatrix(transpose(V), V) ** -1) * transpose(V);
+function getRandomMatrixOnes(samples) {
+    let arr = [];
+    for (let i = 0; i < samples; i++) {
+        arr[i] = (Math.random() > 0.5) ? 1 : -1;
+    }
+    return arr;
+};
 
 
-// продолжаю идти по алгаритму:
-// https://www.pvsm.ru/algoritmy/198772
+let X = getRandomMatrix(10, 5)
+let y = getRandomMatrixOnes(10);
 
-console.log();
+const { w, b } = hoKashyap(X, y)
+
+console.log("w >>>>>> ", w);
+console.log("b >>>>>> ", b);
+
+
+let __result = math.add(math.multiply(w[0], (math.matrix([0, 10]))), b);
+
+__result = __result._data.map((item) => (item) / w[1])
 
 
 
 
+// рисуем линию
 
-//
-// const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
-//
-// const checkPoint = ({ x1, x2, myX, myY, a, b }) => (((myX - x1) / a)**2) + (((myY - x2) / b)**2) <= 1;
-//
-// // генерация точек
-// let index = 0;
-// let points = [];
-// while (index <= 100000) {
-//     points.push({ x1: getRandomArbitrary(0, 10).toFixed(2), x2: getRandomArbitrary(0, 10).toFixed(2) })
-//     ++index;
-// }
-//
-//
-// let verificationPoints = []
-// arr.map((classItem, index) => {
-//     const { x1, x2, a, b, hex, itemsCount } = classItem;
-//     let counter = 0;
-//
-//     verificationPoints.push({
-//         arrItem: index,
-//         points: []
-//     })
-//
-//     points.map((item) => {
-//         const { x1: myX, x2: myY } = item;
-//
-//         if (checkPoint({ x1, x2, myX, myY, a, b }) && counter <= +itemsCount) {
-//             verificationPoints[index].points.push({ myX, myY, hex })
-//             counter++
-//         }
-//     });
-// });
-//
-// let data = JSON.stringify(verificationPoints);
-// fs.writeFileSync('result.json', data);
+const width = 1000;
+const height = 1000;
+
+const canvas = createCanvas(width, height);
+const context = canvas.getContext("2d");
+
+context.fillStyle = "#cccccc";
+context.fillRect(0, 0, width, height);
+
+console.log(((__result[0]) * 10));
+
+context.moveTo(0, ((__result[0]) * 100));
+context.lineTo(1000, ((__result[1]) * 100));
+
+context.stroke();
+
+
+X.map((item, i) => {
+    const [me_x, me_y] = item;
+
+    console.log(me_x, me_y);
+
+    context.fillStyle = y[i] === 1 ? '#ff0000' : '#4ef300';
+    context.fillRect((me_x+5)*100, 1000 - (me_y+5)*100, 4, 4);
+
+})
+
+const buffer = canvas.toBuffer("image/png");
+fs.writeFileSync("image3.png", buffer);
+
 
 
